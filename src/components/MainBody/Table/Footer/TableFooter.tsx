bd30714291c2003c78efa508bac70265ae5baf5a
Andreas Davros <andreasdavros@gmail.com>
2023-02-06 20:46:54 +0200

    small_bug_arrows

--- src/components/MainBody/Table/Footer/TableFooter.tsx
+++ src/components/MainBody/Table/Footer/TableFooter.tsx
@@ -1,18 +1,25 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../../../Context/ContextWrapper";
+import { GlobalContextApi } from "../../../../Context/types";
 import ButtonNavLeft from "./Buttons/ButtonNavLeft";
 import ButtonNavRight from "./Buttons/ButtonNavRight";
 import CurrentPageIsOneToThree from "./MemoizedPagination";
 
 const TableFooter = () => {
 
+	const { filteredRepos } = useContext<GlobalContextApi>(GlobalContext)
+
+	const filteredReposExist = Object.keys(filteredRepos);
+
   return (
 	<tfoot>
 		<tr className="Pagination">
 			<td className="Pagination__Row">
-				<ButtonNavLeft />					
+				{filteredReposExist.length ? <ButtonNavLeft /> : <></>}
 				<div className="Pagination__Numbers">
 					<CurrentPageIsOneToThree />
 				</div>
-				<ButtonNavRight />
+				{filteredReposExist.length ? <ButtonNavRight /> : <></>}
 			</td>
 		</tr>
 	</tfoot>
